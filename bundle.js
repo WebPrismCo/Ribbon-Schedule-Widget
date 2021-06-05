b09f0548a2ccaf5fd2b14021287c7862d9a041e7
webprismdevin <84688190+webprismdevin@users.noreply.github.com>
2021-06-05 14:47:06 -0700

    updates

--- bundle.js
+++ bundle.js
@@ -8,11 +8,18 @@ dayjs.extend(customParseFormat);
 
 
 //load external css
-var link = document.createElement( "link" );
-link.href = "https://raw.githubusercontent.com/WebPrismCo/Ribbon-Schedule-Widget/main/styles.css";
-link.type = "text/css";
-link.rel = "stylesheet";
-link.media = "screen,print";
+var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = "https://cdn.jsdelivr.net/gh/WebPrismCo/Ribbon-Schedule-Widget@main/styles.css";
+    link.media = 'all';
+    head.appendChild(link);
+}
 
 document.getElementsByTagName( "head" )[0].appendChild( link );
 
